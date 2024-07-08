fa896a0e343638160a342a02613ecf78ced1228b
Moeki Kawakami <moeki.kawakami@icloud.com>
2024-07-08 23:50:08 +0900

    Add automatic related note generation

--- app/sidekiq/update_page_job.rb
+++ app/sidekiq/update_page_job.rb
@@ -6,6 +6,7 @@ class UpdatePageJob
     params = JSON.parse(params)
     @page = Page.find_by(id: params["id"].to_i)
     @page.update!(content: "")
+    @page.destinations.destroy_all
     @lang = params["lang"]
     @page.broadcast_update_to(
       "#{dom_id(@page)}",
@@ -30,21 +31,64 @@ class UpdatePageJob
   private
 
   def call_openai
-    OpenAI::Client.new(
-      access_token: ENV["OPENAI_ACCESS_TOKEN"]
-    ).chat(
+    openai = OpenAI::Client.new(access_token: ENV["OPENAI_ACCESS_TOKEN"])
+
+    res = openai.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        response_format: { type: "json_object" },
+        messages: [
+          {
+            role: "user",
+            content: prompt1
+          }
+        ],
+        temperature: 0.4,
+      }
+    )
+
+    @related = JSON.parse(res.dig("choices", 0, "message", "content"))["related"]
+
+    openai.chat(
       parameters: {
         model: "gpt-3.5-turbo",
         messages: [
           {
             role: "user",
-            content: prompt
+            content: prompt2
           }
         ],
-        temperature: 0.7,
+        temperature: 0.4,
         stream: stream_proc,
       }
     )
+
+    @page.broadcast_update_to(
+      "footer-buttons",
+      partial: "pages/loading",
+      target: "footer-buttons-#{@page.id}"
+    )
+
+    res = openai.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        response_format: { type: "json_object" },
+        messages: [
+          {
+            role: "user",
+            content: prompt3
+          }
+        ],
+        temperature: 0.4,
+      }
+    )
+
+    @related = JSON.parse(res.dig("choices", 0, "message", "content"))["related"]
+
+    language_id = Language.find_by(name: @lang).id
+    @related.each do |title|
+      @page.destinations << Page.create(title:, language_id:)
+    end
   end
 
 
@@ -57,19 +101,46 @@ class UpdatePageJob
     end
   end
 
-  def prompt
+  def prompt1
+    case @lang
+    when "ja"
+      <<~MARKDOWN
+        次の「#{@page.title}」に関連するキーワードを20個考えてください。自由に発想していいです。
+
+        出力例(JSON):
+        {
+          related: ["キーワードA", "キーワードB"]
+        }
+      MARKDOWN
+    when "en"
+      <<~MARKDOWN
+        Please think of 20 keywords related to "#{@page.title}". Feel free to brainstorm.
+
+        Output example (JSON):
+        {
+          related: ["Keyword A", "Keyword B"]
+        }
+      MARKDOWN
+    end
+  end
+
+  def prompt2
     case @lang
     when "ja"
       <<~MARKDOWN
         あなたはページを動的に生成するLLMです。「#{@page.title}」について書いてください。より専門的であればあるほど、より具体的な事例が含まれていればいるほど、記事としての価値が高まります。章立てで文章を構成してください。重要な単語やセンテンスは太字にしてください。
 
-        条件:
-          フォーマット: MARKDOWN
-          言語: 日本語
-        文字数:
-          最小: 2000文字
-          最大: 2100文字
+      条件:
+        フォーマット: MARKDOWN
+        言語: 日本語
+      文字数:
+        最小: 2000文字
+        最大: 2100文字
+        キーワード:
+          #{@related.join(",")}
         出力:
+          #{@page.title}
+        続き=>
       MARKDOWN
     when "en"
       <<~MARKDOWN
@@ -83,6 +154,29 @@ class UpdatePageJob
           Max: 2100words
         Output:
       MARKDOWN
+    end
+  end
+
+  def prompt3
+    case @lang
+    when "ja"
+      <<~MARKDOWN
+        「#{@page.title}」に関連するページを10個考えてください。自由に発想していいです。
+
+        出力例(JSON):
+        {
+          related: ["ページA", "ページB", "..."]
+        }
+        MARKDOWN
+    when "en"
+      <<~MARKDOWN
+        Please think of 10 idea cards related to the following idea card "#{@page.title}". Feel free to brainstorm.
+
+        Output example (JSON):
+        {
+          related: ["Page A", "Page B", "..."]
+        }
+        MARKDOWN
     end
   end
 end
