1dbc4b44def9e700ada69d253eeb7c2d5f749514
Moeki Kawakami <moeki.kawakami@icloud.com>
2024-07-09 23:37:16 +0900

    Remove random section

--- app/controllers/langs_controller.rb
+++ app/controllers/langs_controller.rb
@@ -3,32 +3,5 @@ class LangsController < ApplicationController
 
   def show
     @lang = Language.find_by(name: params[:lang])
-    if @lang.name == "ja"
-      @randoms = [
-        "日本の伝統工芸品",
-        "世界の奇妙な祭り",
-        "古代文明の失われた都市",
-        "未来のテクノロジー予測",
-        "珍しい動物とその生態",
-        "歴史に残る名言集",
-        "音楽のジャンルとその特徴",
-        "映画の名シーン解説",
-        "世界の美しい景勝地",
-        "有名な未解決事件"
-      ]
-    else
-      @randoms = [
-        "Traditional Japanese Crafts",
-        "Strange Festivals Around the World",
-        "Lost Cities of Ancient Civilizations",
-        "Future Technology Predictions",
-        "Rare Animals and Their Habitats",
-        "Famous Historical Quotes",
-        "Genres of Music and Their Characteristics",
-        "Iconic Movie Scenes Explained",
-        "Beautiful Scenic Spots Around the World",
-        "Famous Unsolved Mysteries"
-      ]
-    end
   end
 end
